9cd622c8e8e0d8ca94a960b594f8d0d0f312d663
Quan Afoon <quanafoon@gmail.com>
2025-02-14 04:28:50 +0000

    set up flask and layout.html

---
+++ App/views/index.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify, flash
+from App.controllers import create_user, initialize
+from werkzeug.utils import secure_filename
+from flask_jwt_extended import jwt_required
+import os
+import base64
+
+index_views = Blueprint('index_views', __name__, template_folder='../templates')
+
+@index_views.route('/', methods=['GET'])
+def index_page():
+    return render_template('index.html')
+
+@index_views.route('/init', methods=['GET'])
+def init():
+    initialize()
+    return jsonify(message='db initialized!')
+
+@index_views.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({'status':'healthy'})
+
+
