4a7040d2cc8b3af8a1b485c591606a6fd3444a55
quanafoon <quanafoon@gmail.com>
2025-02-21 22:18:32 +0000

    implemented aboutUs.html and minor css changes

--- App/views/index.py
+++ App/views/index.py
@@ -9,9 +9,9 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 def index_page():
     return render_template('index.html')
 
-@index_views.route('/developing', methods=['GET'])
-def developing():
-    return  render_template('in_progress.html')
+@index_views.route('/aboutUsPage', methods=['GET'])
+def about_us_page():
+    return  render_template('aboutUs.html')
 
 @index_views.route('/init', methods=['GET'])
 def init():
