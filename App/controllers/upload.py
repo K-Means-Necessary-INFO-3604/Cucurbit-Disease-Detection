a4516b31d0b532c1d8bc96a2cde9d4d929a37e87
quanafoon <quanafoon@gmail.com>
2025-04-05 21:53:00 -0400

    Final Feature Extraction

--- App/controllers/upload.py
+++ App/controllers/upload.py
@@ -13,8 +13,6 @@ from PIL import Image
 from rembg import remove, new_session
 import geocoder
 import xgboost as xgb
-import torch
-from torchvision import transforms, models
 import onnxruntime as ort
 
 allowed = {'jpg', 'jpeg', 'png'} 
@@ -181,38 +179,6 @@ def get_lat_lng(address):
 model = xgb.Booster()
 model.load_model('ml_models/xgb_model_complete.json') 
 
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-
-
-
-resnet = models.resnet50(pretrained=True)     
-
-
-def extract_features(image):
-    
-    image_tensor = transform(image).unsqueeze(0)
-
-    with torch.no_grad():
-        x = resnet.conv1(image_tensor)
-        x = resnet.bn1(x)
-        x = resnet.relu(x)
-        x = resnet.maxpool(x)
-        x = resnet.layer1(x)
-        x = resnet.layer2(x)
-        x = resnet.layer3(x)
-        x = resnet.layer4(x)
-        x = resnet.avgpool(x)
-        x = torch.flatten(x, 1)
-
-    features = x.cpu().numpy().flatten() 
-    return features
-    
-    
-''' Using an Inference session
 
 session = ort.InferenceSession("ml_models/model.onnx")
 
@@ -226,13 +192,11 @@ def preprocess_image(image):
     return np.expand_dims(img_array, axis=0).astype(np.float32)
 
 def extract_features(image):
-    #image_tensor = transform(image).unsqueeze(0).numpy()
     image_tensor = preprocess_image(image)
     outputs = session.run(None, {"pixel_values": image_tensor})
     output_tensor = outputs[1]
-    print(output_tensor)
     return output_tensor.flatten()
-'''
+
 
 def classify_disease(model, image_features):
     dmatrix = xgb.DMatrix(image_features.reshape(1, -1))
